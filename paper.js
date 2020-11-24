f05626b32f52df9c751ab8245ed76026a8fb5b6e
hemantreddy124421 <74652656+hemantreddy124421@users.noreply.github.com>
2020-11-24 15:50:52 +0530

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,21 @@
+class paper
+{
+ constructor(x,y,r)
+ {
+     var option={
+      isstastic:false,
+      restitution:0.3,
+      friction:0.5,
+      density:1.2  
+
+     }
+   this.x=x;
+   this.y=y;
+   this.r=r
+   this.body=bodies.circle(this.x,this.y.this.r/2,options)
+   world.add(world,this.body);
+} 
+
+
+
+}
